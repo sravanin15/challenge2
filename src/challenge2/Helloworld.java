448219ae67e03422145192b527c034e1dbc8f362
sravanin15 <sravaninuthanapati15@gmail.com>
2021-02-05 12:59:31 +0530

    first commit

---
+++ src/challenge2/Helloworld.java
@@ -0,0 +1,7 @@
+package challenge2;
+
+public class Helloworld {
+public static void main(String[] args) {
+	System.out.println("Helloworld");
+}
+}
---
+++ src/challenge2/subtraction.java
@@ -0,0 +1,9 @@
+package challenge2;
+
+public class subtraction {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	System.out.println(a-b);
+}
+}
